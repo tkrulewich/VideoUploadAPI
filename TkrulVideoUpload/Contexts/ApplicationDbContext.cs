01b033bf9fb2d9e48bd91e76c505317ee47bcaad
Edward Krulewich <ekkyv6@mail.umkc.edu>
2023-06-03 10:02:15 -0500

    add default values to remove null warnings

--- TkrulVideoUpload/Contexts/ApplicationDbContext.cs
+++ TkrulVideoUpload/Contexts/ApplicationDbContext.cs
@@ -5,11 +5,12 @@ using TkrulVideoUpload.Models.Entities;
 public class ApplicationDbContext : IdentityDbContext
 {
 
-    public virtual DbSet<Video> Videos { get; set; }
+    public DbSet<Video> Videos { get; set; }
     
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+        Videos = Set<Video>();
     }
 
     // Additional DbSet properties for your entities can be
--- TkrulVideoUpload/Models/Entities/Video.cs
+++ TkrulVideoUpload/Models/Entities/Video.cs
@@ -11,12 +11,12 @@ public class Video
     public Guid Id { get; set; }
 
     [Required]
-    public string Url { get; set; }
+    public string Url { get; set; } = "";
 
     [ForeignKey("UploaderUser")]
-    public string UploaderUserId { get; set; }
+    public string UploaderUserId { get; set; } = "";
 
-    public IdentityUser UploaderUser { get; set; }
+    public IdentityUser? UploaderUser { get; set; }
 
     [Required]
     public DateTime UploadedDate { get; set; }
